5ea38f9073442e4f071cdb0387441ff7390317cc
Ryan Vieira Souza <ryanvsouza99@gmail.com>
2022-07-12 19:48:19 -0300

    Estrutura da aplicação - Finalizando Sidebar

--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from "phosphor-react";
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
@@ -8,12 +9,21 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1613376023733-0a73315d9b06?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGFuaW1lfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
       ></img>
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/RyanV-Souza.png"
+          alt=""
+        />
+
         <strong> Ryan Vieira</strong>
-        <span> Developer</span>
+        <span>Web Developer</span>
       </div>
 
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
