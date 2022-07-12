7efeb320584b17c3b7a4c359c80511075648fa0e
Ryan Vieira Souza <ryanvsouza99@gmail.com>
2022-07-12 17:54:53 -0300

    Iniciando com React - Propriedades

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -3,9 +3,14 @@ import { Post } from "./Post";
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Ryan Vieira Souza"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque nesciunt
+        culpa aspernatur nobis esse ea, pariatur repudiandae libero? Aspernatur
+        optio, quasi inventore provident qui error fuga ipsa aut distinctio
+        perferendis!"
+      />
+      <Post author="Ryan Vieira" content="23233232!" />
     </div>
   );
 }
